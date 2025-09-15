b4c814d5727a0fa5ed45c3e8a750a0787f3a7241
Mbetania <betaniamarianag@gmail.com>
2025-09-15 17:52:38 -0300

    este cambio es para poder editar el json creando una nueva tarea

--- src/modules/tareas/tareas.routes.js
+++ src/modules/tareas/tareas.routes.js
@@ -9,12 +9,13 @@ import {
     getTareasByPrioridad,
     getTareasByEmpleado,
     getTareasByPaciente,
-    getTareasByFecha
+    getTareasByFecha,
+    testFechas
 } from "./tareas.controller.js";
 
 const router = Router();
 
-// filters
+router.get("/fecha", getTareasByFecha);
 router.get("/estado/:estado", getTareasByEstado);
 router.get("/prioridad/:prioridad", getTareasByPrioridad);
 router.get("/empleado/:empleadoId", getTareasByEmpleado);
