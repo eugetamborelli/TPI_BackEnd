5dd4697c2a602656cd29d08ec7c43e6d86801f99
Mbetania <betaniamarianag@gmail.com>
2025-09-17 11:53:33 -0300

    correccion en logica de app e index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
-import routes from "./routes/index.js";
+import app from "./app.js";
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api", routes);
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running at http://localhost:${PORT}/api/`);
