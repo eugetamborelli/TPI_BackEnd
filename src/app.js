dc160065f5aa0d7ae678f058d9fd14d903f8f970
Euge <eugeniatamborelli@gmail.com>
2025-09-18 15:20:45 -0300

    cambios en rutas que estaban mal enlazadas.

--- src/app.js
+++ src/app.js
@@ -1,43 +1,29 @@
 
 import express from "express";
 import cors from "cors";
-
-//<<<<<<< HEAD
-
 import routes from "./routes/index.js";
-//=======
 import healthRoutes from "./modules/health/health.routes.js";
 import tareasRoutes from "./modules/tareas/tareas.routes.js";
-//<<<<<<< HEAD
-//>>>>>>> 4baf3c5 (add tarea module)
-//=======
 import medicosRoutes from "./modules/medicos/medicos.router.js";
 
 //>>>>>>> 0cd9857 (cambios en rutas que estaban mal enlazadas.)
 
-import healthRoutes from "./modules/health/health.routes.js";
-import tareasRoutes from "./modules/tareas/tareas.routes.js";
-import medicosRoutes from "./modules/medicos/medicos.router.js";
-
-
 const app = express();
+const path = require('path');
 
-// Middlewares globales
 app.use(cors());
 app.use(express.json());
 
 // Rutas
-//<<<<<<< HEAD
+
 app.use("/api", routes);
-
-app.use("/health", healthRoutes);
-app.use("/tareas", tareasRoutes)
-app.use("/api/medicos", medicosRoutes);
-
 //=======
 app.use("/health", healthRoutes);
 //<<<<<<< HEAD
 app.use("tareas", tareasRoutes)
 //>>>>>>> 4baf3c5 (add tarea module)
+//=======
+app.use("/tareas", tareasRoutes)
+//>>>>>>> f238f4a (limpiando el codigo)
 
 export default app;
