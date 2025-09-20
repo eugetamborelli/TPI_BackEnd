801ac3cf77e46113b9314b1b453c50b977a74c71
Matías Spataro <matiasarielspataro@gmail.com>
2025-09-20 20:39:24 -0300

    funciones de utils cambiadas a asíncronas y con path portable

--- src/utils/file.utils.js
+++ src/utils/file.utils.js
@@ -1,14 +1,18 @@
-import fs from "fs";
+import { promises as fs } from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
 
-export const readJsonFile = (fileName) => {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const readJsonFile = async (fileName) => {
     const filePath = getFilePath(fileName);
     try {
-        if (!fs.existsSync(filePath)) {
-            fs.writeFileSync(filePath, JSON.stringify([], null, 2));
-            return [];
-        }
-        const data = fs.readFileSync(filePath, "utf8");
+        await fs.access(filePath).catch(async () => {
+        await fs.writeFile(filePath, JSON.stringify([], null, 2));
+        });
+
+        const data = await fs.readFile(filePath, "utf8");
         return JSON.parse(data || "[]");
     } catch (error) {
         console.error(`Error reading ${fileName} file:`, error);
@@ -16,10 +20,10 @@ export const readJsonFile = (fileName) => {
     }
 }
 
-export const writeJsonFile = (fileName, data) => {
+export const writeJsonFile = async (fileName, data) => {
     const filePath = getFilePath(fileName);
     try {
-        fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+        await fs.writeFile(filePath, JSON.stringify(data, null, 2));
         return true;
     } catch (error) {
         console.error(`Error writing ${fileName} file:`, error);
@@ -29,5 +33,5 @@ export const writeJsonFile = (fileName, data) => {
 
 //Obtiene el path de los archivos de BD
 const getFilePath = (fileName) => {
-    return path.resolve(`src/databases/${fileName}.json`);
+    return path.join(__dirname, "../databases", `${fileName}.json`);
 }
