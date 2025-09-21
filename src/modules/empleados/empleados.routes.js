3b65b4282d05b59cefed9eeb4dcb0ae89e1965ab
Mbetania <betaniamarianag@gmail.com>
2025-09-21 18:44:13 -0300

    change put for patch

--- src/modules/empleados/empleados.routes.js
+++ src/modules/empleados/empleados.routes.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
-import empleadosController from "./empleados.controller.js"; // export default new EmpleadosController()
+import empleadosController from "./empleados.controller.js";
+import { validarCampos } from "./empleados.middleware.js";
 
 const router = Router();
 
 router.get("/rol/:rol", empleadosController.getEmpleadosByRol);
 router.get("/area/:area", empleadosController.getEmpleadosByArea);
+router.get("/dni/:dni", empleadosController.getEmpleadoByDni);
 router.get("/", empleadosController.getEmpleados);
 router.get("/:id", empleadosController.getEmpleado);
 router.post("/", empleadosController.addEmpleado);
-router.put("/:id", empleadosController.editEmpleado);
+router.patch("/:id", validarCampos, empleadosController.editEmpleado);
 router.delete("/:id", empleadosController.removeEmpleado);
 
 export default router;
